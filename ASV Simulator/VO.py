c27ecdbb5f29635fec537c6ad2c5803e0569ca9a
Arild V <valderalv@hotmail.com>
2021-03-04 13:43:59 +0100

    Created Velocity Obstacle class

---
+++ ASV Simulator/VO.py
@@ -0,0 +1,12 @@
+import sys, time
+import heapq
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from map import Map
+from vessel import Vessel
+from utils import Controller, PriorityQueue
+
+from matplotlib2tikz import save as tikz_save
+
